35d8218b9cbf4fdb0da1195014c6577adee1d696
Bigboss <mtse431@gmail.com>
2023-12-24 21:01:48 +1030

    Creates Function to check game ends

--- script.js
+++ script.js
@@ -5,10 +5,40 @@ const gameContainerEl = document.querySelector(".game__container");
 const gameArenaEl = document.querySelector(".game__arena");
 const playerTurnEl = document.querySelector(".player__turn");
 let currentPlayer = "x";
+const gameMaps = {
+  row0: [],
+  col0: [],
+  row1: [],
+  col1: [],
+  row2: [],
+  col2: [],
+  slash: [],
+  backslash: [],
+};
+
 const displayGame = function () {
   header.classList.add("active");
   playerForm.classList.remove("active");
   gameContainerEl.classList.add("active");
+};
+
+const gameCheck = function () {
+  Object.entries(gameMaps).forEach(entry => {
+    const [_, value] = entry;
+    if (value.length === 3 && new Set(value).size === 1) {
+      console.log("GAME END!!");
+    }
+  });
+};
+
+const inputOrganize = function (element) {
+  const symbol = element.querySelector("img").alt;
+  gameMaps[element.dataset.row].push(symbol);
+  gameMaps[element.dataset.column].push(symbol);
+  gameMaps[element.dataset.slash] &&
+    gameMaps[element.dataset.slash].push(symbol);
+  gameMaps[element.dataset.backslash] &&
+    gameMaps[element.dataset.backslash].push(symbol);
 };
 
 const changeTurn = function () {
@@ -18,10 +48,13 @@ const changeTurn = function () {
 };
 
 const placeMark = function (element) {
-  const mark = `<img src="./assets/icon-${currentPlayer}.svg" alt="icon ${currentPlayer}">`;
+  const mark = `<img src="./assets/icon-${currentPlayer}.svg" alt="${currentPlayer}">`;
   element.insertAdjacentHTML("beforeend", mark);
 
   element.disabled = true;
+
+  inputOrganize(element);
+  gameCheck();
 };
 
 playButtonsEl.addEventListener("click", function (e) {
