c60a319910ba712ab45b03303891f5be27966533
Bigboss <mtse431@gmail.com>
2023-12-24 22:10:54 +1030

    Creates Function to determine player selection

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const gameContainerEl = document.querySelector(".game__container");
 const gameArenaEl = document.querySelector(".game__arena");
 const playerTurnEl = document.querySelector(".player__turn");
 let currentPlayer = "x";
+const playerOptions = [...document.querySelectorAll("input[type='radio']")];
+
+const player1 = {
+  name: "",
+  number: "1",
+  mark: "",
+  score: "",
+};
+
+const player2 = {
+  name: "",
+  number: "2",
+  mark: "",
+  score: "",
+};
+
 const gameMaps = {
   row0: [],
   col0: [],
@@ -20,6 +36,22 @@ const displayGame = function () {
   header.classList.add("active");
   playerForm.classList.remove("active");
   gameContainerEl.classList.add("active");
+};
+
+const playerSelection = function (player) {
+  if (player) {
+    player1.name = "p1";
+    player2.name = "p2";
+  } else {
+    player1.name = "you";
+    player2.name = "cpu";
+  }
+  player1.mark = playerOptions.find(option => option.checked).value;
+  player2.mark = playerOptions.find(option => !option.checked).value;
+  const p1Board = document.querySelector(`#board-${player1.mark}`);
+  const challengerBoard = document.querySelector(`#board-${player2.mark}`);
+  p1Board.textContent = `${player1.mark} (${player1.name})`;
+  challengerBoard.textContent = `${player2.mark} (${player2.name})`;
 };
 
 const gameCheck = function () {
@@ -63,9 +95,11 @@ playButtonsEl.addEventListener("click", function (e) {
   displayGame();
 
   if (element.classList.contains("btn") && element.id === "cpu") {
+    playerSelection();
   }
 
   if (element.classList.contains("btn") && element.id === "other--player") {
+    playerSelection(true);
   }
 });
 
