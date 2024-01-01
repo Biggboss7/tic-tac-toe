5708a6e8931af4922f34c1d63a088b71e55b54dc
Bigboss <mtse431@gmail.com>
2024-01-01 16:19:50 +1030

    Commit of Tic-Tac-Toe v1.0

--- script.js
+++ script.js
@@ -20,196 +20,298 @@ let clickedButtonCounter = 0;
 let winningMark = "";
 let tieScore = 0;
 const boardTieEl = document.querySelector("#board-tie");
+let vsPlayer = "";
+let p1Board;
+let challengerBoard;
+class Player {
+  constructor(name, alias) {
+    this.name = name;
+    this.alias = alias;
+    this.score = 0;
+    this.mark = "";
+  }
 
-const player1 = {
-  name: "",
-  number: "1",
-  mark: "",
-  score: 0,
-};
+  changeTurn() {
+    gameArenaEl.classList.toggle("o");
+    currentPlayer = gameArenaEl.classList[gameArenaEl.classList.length - 1];
+    playerTurnEl.style.backgroundImage = `url("./assets/icon-${currentPlayer}.svg")`;
+  }
 
-const player2 = {
-  name: "",
-  number: "2",
-  mark: "",
-  score: 0,
-};
+  placeMark(element) {
+    const mark = `<img src="./assets/icon-${currentPlayer}.svg" alt="${currentPlayer}">`;
+    element.insertAdjacentHTML("beforeend", mark);
 
-const players = [player1, player2];
+    element.disabled = true;
 
-const gameMaps = {
-  row0: [],
-  col0: [],
-  row1: [],
-  col1: [],
-  row2: [],
-  col2: [],
-  slash: [],
-  backslash: [],
+    clickedButtonCounter++;
+
+    ticTacToe.inputOrganize(element);
+    ticTacToe.gameCheck();
+    this.changeTurn();
+  }
+}
+
+class Computer extends Player {
+  generateMove() {
+    const randomIndex = function () {
+      return Math.floor(Math.random() * gameBlocksEl.length);
+    };
+    let possibleBtn = gameBlocksEl[randomIndex()];
+    while (possibleBtn.disabled === true) {
+      possibleBtn = gameBlocksEl[randomIndex()];
+    }
+
+    setTimeout(() => this.placeMark(possibleBtn), 200);
+  }
+
+  analysisMove() {
+    if (clickedButtonCounter <= 1) this.generateMove();
+    else {
+      let needDefense;
+      for (const row in ticTacToe.gameMaps) {
+        // Analysis Player Move (Defensive Mode)
+        if (
+          ticTacToe.gameMaps[row].length === 2 &&
+          ticTacToe.gameMaps[row].indexOf(player1.mark) !==
+            ticTacToe.gameMaps[row].lastIndexOf(player1.mark)
+        ) {
+          needDefense = true;
+          const type = RegExp(/[a-z]+/, "g").exec(row)[0];
+          const possibleBlock = [
+            ...document.querySelectorAll(`button[data-${type}="${row}"]`),
+          ].find(block => block.disabled === false);
+          setTimeout(() => this.placeMark(possibleBlock), 200);
+          break;
+        }
+      }
+      // Analysis Player Move (Offensive Mode)
+      if (!needDefense) {
+        let neutralMove;
+        for (const row in ticTacToe.gameMaps) {
+          if (
+            ticTacToe.gameMaps[row].includes(this.mark) &&
+            !ticTacToe.gameMaps[row].includes(player1.mark)
+          ) {
+            const type = RegExp(/[a-z]+/, "g").exec(row)[0];
+            const possibleBlock = [
+              ...document.querySelectorAll(`button[data-${type}="${row}"]`),
+            ].find(block => block.disabled === false);
+            setTimeout(() => this.placeMark(possibleBlock), 200);
+            neutralMove = false;
+            break;
+          } else {
+            neutralMove = true;
+          }
+        }
+        if (neutralMove) {
+          setTimeout(
+            () =>
+              this.placeMark(
+                [...gameBlocksEl].find(block => block.disabled === false)
+              ),
+            200
+          );
+        }
+      }
+    }
+  }
+}
+
+const player1 = new Player("player 1", "p1");
+const player2 = new Player("player 2", "p2");
+const cpu = new Computer("computer", "cpu");
+
+const players = [player1, player2, cpu];
+
+// Game's World
+const ticTacToe = {
+  gameMaps: {
+    row0: [],
+    col0: [],
+    row1: [],
+    col1: [],
+    row2: [],
+    col2: [],
+    slash: [],
+    backslash: [],
+  },
+
+  displayGame(active = true) {
+    if (active) {
+      header.classList.add("active");
+      playerForm.classList.remove("active");
+      gameContainerEl.classList.add("active");
+    } else {
+      header.classList.remove("active");
+      playerForm.classList.add("active");
+      gameContainerEl.classList.remove("active");
+    }
+  },
+
+  setDataPlayer() {
+    player1.mark = playerOptions.find(option => option.checked).value;
+
+    p1Board = document.querySelector(`#board-${player1.mark}`);
+
+    if (vsPlayer) {
+      player2.mark = playerOptions.find(option => !option.checked).value;
+      challengerBoard = document.querySelector(`#board-${player2.mark}`);
+      challengerBoard.textContent = `${player2.mark} (${player2.alias})`;
+    } else {
+      player1.alias = "you";
+      cpu.mark = playerOptions.find(option => !option.checked).value;
+      challengerBoard = document.querySelector(`#board-${cpu.mark}`);
+      challengerBoard.textContent = `${cpu.mark} (${cpu.alias})`;
+    }
+    p1Board.textContent = `${player1.mark} (${player1.alias})`;
+  },
+
+  winningDisplay() {
+    winningMark = document.createElement("img");
+    winningMark.setAttribute("src", `./assets/icon-${winner.mark}.svg`);
+    winningMark.setAttribute("alt", "winning mark");
+
+    winningMarkWrapperEl.prepend(winningMark);
+    winner.score += 1;
+    document.querySelector(
+      `#board-${winner.mark} + strong`
+    ).textContent = `${winner.score}`;
+    overlayEl.classList.add("active");
+    winnerMessageEl.textContent = `${winner.name} wins!`;
+
+    for (const row in this.gameMaps) this.gameMaps[row] = [];
+  },
+
+  tieDisplay() {
+    overlayEl.classList.add("active");
+    roundTakerContentEl.textContent = "";
+    winnerMessageEl.textContent = "round tied!";
+    tieScore++;
+    boardTieEl.textContent = tieScore;
+
+    for (const row in this.gameMaps) this.gameMaps[row] = [];
+  },
+
+  gameCheck() {
+    for (const key in this.gameMaps) {
+      if (
+        this.gameMaps[key].length === 3 &&
+        new Set(this.gameMaps[key]).size === 1
+      ) {
+        winner = players.find(
+          player => player.mark === [...new Set(this.gameMaps[key])][0]
+        );
+        this.winningDisplay();
+        break;
+      }
+    }
+    if (clickedButtonCounter === 9 && !winner) this.tieDisplay();
+  },
+
+  inputOrganize(element) {
+    const symbol = element.querySelector("img").alt;
+    this.gameMaps[element.dataset.row].push(symbol);
+    this.gameMaps[element.dataset.col].push(symbol);
+    this.gameMaps[element.dataset.slash] &&
+      this.gameMaps[element.dataset.slash].push(symbol);
+    this.gameMaps[element.dataset.backslash] &&
+      this.gameMaps[element.dataset.backslash].push(symbol);
+  },
+
+  resetGame() {
+    winner = "";
+
+    for (const el in this.gameMaps) {
+      this.gameMaps[el] = [];
+    }
+
+    for (const block of gameBlocksEl) {
+      const mark = block.children[0];
+      mark && block.removeChild(mark);
+      block.disabled = false;
+    }
+
+    currentPlayer = "x";
+    playerTurnEl.style.backgroundImage = `url("./assets/icon-${currentPlayer}.svg")`;
+    gameArenaEl.classList.contains("o") && gameArenaEl.classList.remove("o");
+
+    clickedButtonCounter = 0;
+
+    winningMark && winningMarkWrapperEl.removeChild(winningMark);
+    winningMark = "";
+
+    roundTakerContentEl.textContent = "takes the round!";
+
+    if (clickedButtonCounter === 0 && cpu.mark === "x") {
+      cpu.generateMove();
+    }
+  },
+
+  quitGame() {
+    this.displayGame(false);
+    overlayEl.classList.remove("active");
+
+    player1.alias = "p1";
+
+    players.forEach(player => {
+      player.score = 0;
+      player.mark = "";
+    });
+    tieScore = 0;
+    for (const board of scoreBoardEl) {
+      board.textContent = "0";
+    }
+
+    vsPlayer = "";
+
+    this.resetGame();
+  },
+
+  nextRound() {
+    overlayEl.classList.remove("active");
+    this.resetGame();
+  },
 };
 
 // Functions
-const displayGame = function () {
-  header.classList.add("active");
-  playerForm.classList.remove("active");
-  gameContainerEl.classList.add("active");
-};
-
-const playerSelection = function (player) {
-  if (player) {
-    player1.name = "p1";
-    player2.name = "p2";
-  } else {
-    player1.name = "you";
-    player2.name = "cpu";
-  }
-  player1.mark = playerOptions.find(option => option.checked).value;
-  player2.mark = playerOptions.find(option => !option.checked).value;
-  const p1Board = document.querySelector(`#board-${player1.mark}`);
-  const challengerBoard = document.querySelector(`#board-${player2.mark}`);
-  p1Board.textContent = `${player1.mark} (${player1.name})`;
-  challengerBoard.textContent = `${player2.mark} (${player2.name})`;
-};
-
-const winningDisplay = function () {
-  winningMark = document.createElement("img");
-  winningMark.setAttribute("src", `./assets/icon-${winner.mark}.svg`);
-  winningMark.setAttribute("alt", "winning mark");
-
-  winningMarkWrapperEl.prepend(winningMark);
-  winner.score += 1;
-  document.querySelector(
-    `#board-${winner.mark} + strong`
-  ).textContent = `${winner.score}`;
-  overlayEl.classList.add("active");
-  winnerMessageEl.textContent = `player ${winner.number} wins!`;
-};
-
-const tieDisplay = function () {
-  overlayEl.classList.add("active");
-  roundTakerContentEl.textContent = "";
-  winnerMessageEl.textContent = "round tied!";
-  tieScore++;
-  boardTieEl.textContent = tieScore;
-};
-
-const gameCheck = function () {
-  for (const key in gameMaps) {
-    if (gameMaps[key].length === 3 && new Set(gameMaps[key]).size === 1) {
-      winner = players.find(
-        player => player.mark === [...new Set(gameMaps[key])][0]
-      );
-      winningDisplay();
-    }
-  }
-  if (clickedButtonCounter === 9 && !winner) tieDisplay();
-};
-
-const inputOrganize = function (element) {
-  const symbol = element.querySelector("img").alt;
-  gameMaps[element.dataset.row].push(symbol);
-  gameMaps[element.dataset.column].push(symbol);
-  gameMaps[element.dataset.slash] &&
-    gameMaps[element.dataset.slash].push(symbol);
-  gameMaps[element.dataset.backslash] &&
-    gameMaps[element.dataset.backslash].push(symbol);
-};
-
-const changeTurn = function () {
-  gameArenaEl.classList.toggle("o");
-  currentPlayer = gameArenaEl.classList[gameArenaEl.classList.length - 1];
-  playerTurnEl.style.backgroundImage = `url("./assets/icon-${currentPlayer}.svg")`;
-};
-
-const placeMark = function (element) {
-  const mark = `<img src="./assets/icon-${currentPlayer}.svg" alt="${currentPlayer}">`;
-  element.insertAdjacentHTML("beforeend", mark);
-
-  element.disabled = true;
-
-  clickedButtonCounter++;
-
-  inputOrganize(element);
-  gameCheck();
-};
-
-const resetGame = function () {
-  winner = "";
-
-  for (const el in gameMaps) {
-    gameMaps[el] = [];
-  }
-
-  for (const block of gameBlocksEl) {
-    const mark = block.children[0];
-    mark && block.removeChild(mark);
-    block.disabled = false;
-  }
-
-  currentPlayer = "x";
-  playerTurnEl.style.backgroundImage = `url("./assets/icon-${currentPlayer}.svg")`;
-  gameArenaEl.classList.contains("o") && gameArenaEl.classList.remove("o");
-
-  clickedButtonCounter = 0;
-
-  winningMark && winningMarkWrapperEl.removeChild(winningMark);
-  winningMark = "";
-
-  roundTakerContentEl.textContent = "takes the round!";
-};
-
-const quitGame = function () {
-  header.classList.remove("active");
-  playerForm.classList.add("active");
-  gameContainerEl.classList.remove("active");
-  overlayEl.classList.remove("active");
-
-  for (const board of scoreBoardEl) {
-    board.textContent = "0";
-  }
-
-  players.forEach(player => {
-    player.name = "";
-    player.score = 0;
-    player.mark = "";
-  });
-
-  tieScore = 0;
-  boardTieEl.textContent = 0;
-
-  resetGame();
-};
-
-const nextRound = function () {
-  overlayEl.classList.remove("active");
-  resetGame();
-};
 
 // Button Event Listener
 
 playButtonsEl.addEventListener("click", function (e) {
   const element = e.target;
 
-  displayGame();
-
   if (element.classList.contains("btn") && element.id === "cpu") {
-    playerSelection();
+    vsPlayer = false;
+    ticTacToe.displayGame();
+    ticTacToe.setDataPlayer();
+
+    // Determine First Player's Move
+    if (clickedButtonCounter === 0 && cpu.mark === "x") {
+      cpu.generateMove();
+    }
   }
 
   if (element.classList.contains("btn") && element.id === "other--player") {
-    playerSelection(true);
+    vsPlayer = true;
+    ticTacToe.displayGame();
+    ticTacToe.setDataPlayer();
   }
 });
 
 gameArenaEl.addEventListener("click", function (e) {
   if (e.target.classList.contains("btn")) {
-    placeMark(e.target);
-    changeTurn();
+    if (vsPlayer) {
+      const firstPlayer = players.find(player => player.mark === "x");
+      firstPlayer.placeMark(e.target);
+    } else {
+      player1.placeMark(e.target);
+      clickedButtonCounter < 9 && cpu.analysisMove();
+    }
   }
 });
 
-btnQuitEl.addEventListener("click", quitGame);
+btnQuitEl.addEventListener("click", ticTacToe.quitGame.bind(ticTacToe));
 
-btnReplayEl.addEventListener("click", resetGame);
+btnReplayEl.addEventListener("click", ticTacToe.resetGame.bind(ticTacToe));
 
-btnNextRoundEl.addEventListener("click", nextRound);
+btnNextRoundEl.addEventListener("click", ticTacToe.nextRound.bind(ticTacToe));
